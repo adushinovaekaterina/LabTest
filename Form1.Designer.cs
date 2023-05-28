45da91f18f10c99969bec6b41ea079b54a4e1432
adushinovaekaterina <adushinova2002@mail.ru>
2023-05-28 17:26:40 +0800

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picCanvas.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.picCanvas.Location = new System.Drawing.Point(0, 0);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(244, 361);
+            this.picCanvas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picCanvas.TabIndex = 1;
+            this.picCanvas.TabStop = false;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 3;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(9, 33);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            13,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "см";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введите радиус:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(5, 117);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(124, 45);
+            this.trackBar1.SmallChange = 2;
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 5;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Площадь \r\nзаштрихованной \r\nобласти:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 246);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(103, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Тут будет площадь";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Location = new System.Drawing.Point(250, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(135, 361);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Изменение размера:\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.picCanvas);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(400, 400);
+            this.Name = "Form1";
+            this.Text = "Программа";
+            this.ResizeBegin += new System.EventHandler(this.Form1_ResizeBegin);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private int vertices = 4; // количество вершин
+        private double pix = 37.936; // размер фигур
+        int _oldWidth, _oldHeight; // 
+        int _oldTrackBar; // 
+        float proportion = 1f; //
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            double radius = (double)numericUpDown1.Value * pix; // радиус
+            DrawPolygon_Ellipse(radius);
+            _oldTrackBar = trackBar1.Value;
+        }
+        private void DrawPolygon_Ellipse(double radius)
+        {
+            int angle = 18;
+
+            Point center = new Point(picCanvas.Width / 2, picCanvas.Height / 2);
+
+            Point[] verticies = CalculateVertices(radius, angle, center);
+
+            Bitmap polygon = new Bitmap(picCanvas.ClientSize.Width, picCanvas.ClientSize.Height);
+
+            using (Graphics g = Graphics.FromImage(polygon))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                HatchBrush hBrush = new HatchBrush(
+                  HatchStyle.BackwardDiagonal,
+                  Color.Red,
+                  Color.White);
+
+                g.DrawEllipse(Pens.Black, center.X - (int)radius, center.Y - (int)radius, 2 * (int)radius, 2 * (int)radius);
+                g.FillEllipse(hBrush, center.X - (int)radius, center.Y - (int)radius, 2 * (int)radius, 2 * (int)radius);
+
+                g.DrawPolygon(Pens.Black, verticies);
+
+                SolidBrush brush = new SolidBrush(Color.White);
+                g.FillPolygon(brush, verticies);
+
+                //g.DrawEllipse(Pens.Blue, center.X, center.Y-50, 3, 3);
+            }
+
+            picCanvas.Image = polygon;
+
+            label4.Text = "S=" + AreaCalculation((double)numericUpDown1.Value).ToString();
+        }
+
+        private Point[] CalculateVertices(double radius, int startingAngle, Point center)
+        {
+            List<Point> points = new List<Point>();
+            float step = 360.0f / vertices;
+            float angle = startingAngle;
+            for (double i = startingAngle; i < startingAngle + 360.0; i += step) //go in a circle
+            {
+                Point xy = new Point();
+                double radians = angle * Math.PI / 180.0;
+                xy.X = (int)(Math.Cos(radians) * radius + center.X);
+                xy.Y = (int)(Math.Sin(-radians) * radius + center.Y);
+                points.Add(xy);
+                angle += step;
+            }
+            return points.ToArray();
+        }
+
+        private double AreaCalculation(double radius)
+        {
+            return Math.Round(AreaCalculationEllipse(radius) - AreaCalculationPolygon(radius), 3);
+        }
+
+        private double AreaCalculationEllipse(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        private double AreaCalculationPolygon(double radius)
+        {
+            double a = radius * 10 / (Math.Sqrt(50 + 10 * Math.Sqrt(5)));
+            return Math.Sqrt(25 + 10 * Math.Sqrt(5)) / 4 * Math.Pow(a, 2);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            radiusChanged(trackBar1.Value, _oldTrackBar);
+            double radius = (double)numericUpDown1.Value * pix;
+            DrawPolygon_Ellipse(radius);
+            _oldTrackBar = trackBar1.Value;
+        }
+
+        private void radiusChanged(int a, int b)
+        {
+            if (a > b)
+            {
+                numericUpDown1.Value = numericUpDown1.Value + (numericUpDown1.Value * (decimal)0.1);
+            }
+            else if (a < b)
+            {
+                numericUpDown1.Value = numericUpDown1.Value - (numericUpDown1.Value * (decimal)0.1);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            double radius = (double)numericUpDown1.Value * pix;
+            DrawPolygon_Ellipse(radius);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            double radius = (double)numericUpDown1.Value * pix;
+            DrawPolygon_Ellipse(radius);
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            _oldWidth = Width;
+            _oldHeight = Height;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            Control control = (Control)sender;
+            // если изменилась ширина с прошлого раза
+            if (_oldWidth != Width)
+            {
+                control.Size = new Size(control.Size.Width, (int)(control.Size.Width * 1f / proportion));
+                double koeff = (double)control.Size.Width / (double)_oldWidth;
+                numericUpDown1.Value = (numericUpDown1.Value * (decimal)koeff);
+
+            }
+            // если изменилась высота с прошлого раза
+            if (_oldHeight != Height)
+            {
+                control.Size = new Size((int)(control.Size.Height * proportion), control.Size.Height);
+                double koeff = (double)control.Size.Height / (double)_oldHeight;
+                numericUpDown1.Value = numericUpDown1.Value * (decimal)koeff;
+            }
+        }
+    }
+}
